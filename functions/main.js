2f660d3a8e6dacdd81dd135169083823599eb2cb
David Rosenberger <drosenberger84@gmail.com>
2019-04-27 20:51:10 -0400

    styled footer

--- functions/main.js
+++ functions/main.js
@@ -10,6 +10,43 @@ $(function(){
     });
 });
 
+//  Smooth Scroll
+// ===============
+$(function(){
+  $("#top-sec").click(function(){
+      $path = $("#top").offset().top;
+
+      $('html, body').animate({scrollTop:$path}, 2000);
+  });
+
+  $("#about-sec").click(function(){
+      $path = $("#about").offset().top;
+
+      $('html, body').animate({scrollTop:$path}, 1000);
+  });
+
+  $("#portfolio-sec").click(function(){
+      $path = $("#portfolio").offset().top;
+
+      $('html, body').animate({scrollTop: $path}, 2000);
+  });
+
+  $("#connect-sec").click(function(){
+    $path = $("#connect").offset().top;
+
+    $('html, body').animate({scrollTop: $path}, 2000);
+  });
+
+  $("#portfolio-slide").click(function(){
+    $path = $("#portfolio").offset().top;
+
+    $('html, body').animate({scrollTop: $path}, 2000);
+  });
+});
+
+
+//   Testimonals
+// ===============
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -32,4 +69,14 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
+}
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("mainMenu").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("mainMenu").style.width = "0%";
 }
